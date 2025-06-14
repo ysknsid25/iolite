f76dde11c13db86e5ebbcd70f7f3d2c750fe8df9
ysknsid25 <kengo071225@gmail.com>
2025-06-14 23:21:36 +0900

    #47 - IntegerCharacter

---
+++ src/main/kotlin/IntegerCharacter.kt
@@ -0,0 +1,31 @@
+package inorin
+
+@JvmInline
+value class IntegerCharacter private constructor(private val value: String) : StringValueObject {
+    override fun get(): String {
+        return value
+    }
+
+    fun isPositive(): Boolean {
+        return value.toIntOrNull()?.let { it > 0 } ?: false
+    }
+
+    fun isNegative(): Boolean {
+        return value.toIntOrNull()?.let { it < 0 } ?: false
+    }
+
+    fun isZero(): Boolean {
+        return value.toIntOrNull()?.let { it == 0 } ?: false
+    }
+
+    companion object {
+        fun of(input: String): IntegerCharacter {
+            require(
+                Regex("^[+-]?(0|[1-9][0-9]*)\$").matches(input)
+            ) {
+                "Invalid Integer Charactor: $input"
+            }
+            return IntegerCharacter(input)
+        }
+    }
+}
