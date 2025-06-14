2fbff460feb917b01f3ea80316daa3c102bbcfdf
ysknsid25 <kengo071225@gmail.com>
2025-06-14 12:18:38 +0900

    chore: detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
+import io.gitlab.arturbosch.detekt.Detekt
+
 plugins {
     kotlin("jvm") version "2.0.21"
+    id("io.gitlab.arturbosch.detekt") version "1.23.8"
 }
 
 group = "inorin"
@@ -15,8 +18,26 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.10.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.0")
+    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.23.1")
+}
+
+detekt{
+    config.setFrom("${rootProject.projectDir}/config/detekt/detekt.yml")
+    buildUponDefaultConfig = true
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks {
+    withType<Detekt> {
+        reports {
+            html.required.set(true)
+            html.outputLocation.set(file("${rootProject.projectDir}/reports/detekt.html"))
+            xml.required.set(false)
+            txt.required.set(false)
+            sarif.required.set(false)
+        }
+    }
 }
--- src/test/kotlin/personal/EmailTest.kt
+++ src/test/kotlin/personal/EmailTest.kt
@@ -1,87 +1,92 @@
 package personal
 
 import inorin.personal.Email
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
 class EmailTest {
     @ParameterizedTest
-    @ValueSource(strings = [
-        "email@domain.com",
-        "firstname.lastname@domain.com",
-        "email@subdomain.domain.com",
-        "firstname+lastname@domain.com",
-        "1234567890@domain.com",
-        "email@domain-one.com",
-        "_______@domain.com",
-        "email@domain.name",
-        "email@domain.co.jp",
-        "firstname-lastname@domain.com",
-        "very.common@example.com",
-        "disposable.style.email.with+symbol@example.com",
-        "other.email-with-hyphen@example.com",
-        "fully-qualified-domain@example.com",
-        "user.name+tag+sorting@example.com",
-        "x@example.com",
-        "mojojojo@asdf.example.com",
-        "example-indeed@strange-example.com",
-        "example@s.example",
-        "user-@example.org",
-        "user@my-example.com",
-        "a@b.cd",
-        "work+user@mail.com",
-        "tom@test.te-st.com",
-        "something@subdomain.domain-with-hyphens.tld",
-        "common'name@domain.com",
-        "francois@etu.inp-n7.fr"
-    ])
+    @ValueSource(
+        strings = [
+            "email@domain.com",
+            "firstname.lastname@domain.com",
+            "email@subdomain.domain.com",
+            "firstname+lastname@domain.com",
+            "1234567890@domain.com",
+            "email@domain-one.com",
+            "_______@domain.com",
+            "email@domain.name",
+            "email@domain.co.jp",
+            "firstname-lastname@domain.com",
+            "very.common@example.com",
+            "disposable.style.email.with+symbol@example.com",
+            "other.email-with-hyphen@example.com",
+            "fully-qualified-domain@example.com",
+            "user.name+tag+sorting@example.com",
+            "x@example.com",
+            "mojojojo@asdf.example.com",
+            "example-indeed@strange-example.com",
+            "example@s.example",
+            "user-@example.org",
+            "user@my-example.com",
+            "a@b.cd",
+            "work+user@mail.com",
+            "tom@test.te-st.com",
+            "something@subdomain.domain-with-hyphens.tld",
+            "common'name@domain.com",
+            "francois@etu.inp-n7.fr"
+        ]
+    )
     fun `should create Email instance for valid email`(validEmail: String) {
         val email = Email.of(validEmail)
         assertEquals(validEmail.trim().lowercase(), email.get())
     }
 
     @ParameterizedTest
-    @ValueSource(strings = [
-        "francois@@etu.inp-n7.fr",
-        "\"email\"@domain.com",
-        "\"john..doe\"@example.org",
-        "a,b@domain.com",
-        "plainaddress",
-        "@domain.com",
-        "email.domain.com",
-        "email@domain@domain.com",
-        ".email@domain.com",
-        "email.@domain.com",
-        "email..email@domain.com",
-        "あいうえお@domain.com",
-        "email@domain",
-        "email@-domain.com",
-        "email@111.222.333.44444",
-        "email@domain..com",
-        "Abc.example.com",
-        "A@b@c@example.com",
-        "colin..hacks@domain.com",
-        "a\"b(c)d,e:f;g<h>i[j\\k]l@example.com",
-        "just\"not\"right@example.com",
-        "this is\"not\\allowed@example.com",
-        "this\\ still\\\"not\\\\allowed@example.com",
-        "invalid@-start.com",
-        "invalid@end.com-",
-        "a.b@c.d",
-        "invalid@[1.1.1.-1]",
-        "invalid@[68.185.127.196.55]",
-        "temp@[192.168.1]",
-        "temp@[9.18.122.]",
-        "double..point@test.com",
-        "asdad@test..com",
-        "asdad@hghg...sd...au",
-        "asdad@hghg........au",
-        "invalid@[256.2.2.48]",
-        "invalid@[999.465.265.1]",
-        "test@.com",
-        "aaaaaaaaaaaaaaalongemailthatcausesregexDoSvulnerability@test.c"
-    ])
+    @ValueSource(
+        strings = [
+            "francois@@etu.inp-n7.fr",
+            "\"email\"@domain.com",
+            "\"john..doe\"@example.org",
+            "a,b@domain.com",
+            "plainaddress",
+            "@domain.com",
+            "email.domain.com",
+            "email@domain@domain.com",
+            ".email@domain.com",
+            "email.@domain.com",
+            "email..email@domain.com",
+            "あいうえお@domain.com",
+            "email@domain",
+            "email@-domain.com",
+            "email@111.222.333.44444",
+            "email@domain..com",
+            "Abc.example.com",
+            "A@b@c@example.com",
+            "colin..hacks@domain.com",
+            "a\"b(c)d,e:f;g<h>i[j\\k]l@example.com",
+            "just\"not\"right@example.com",
+            "this is\"not\\allowed@example.com",
+            "this\\ still\\\"not\\\\allowed@example.com",
+            "invalid@-start.com",
+            "invalid@end.com-",
+            "a.b@c.d",
+            "invalid@[1.1.1.-1]",
+            "invalid@[68.185.127.196.55]",
+            "temp@[192.168.1]",
+            "temp@[9.18.122.]",
+            "double..point@test.com",
+            "asdad@test..com",
+            "asdad@hghg...sd...au",
+            "asdad@hghg........au",
+            "invalid@[256.2.2.48]",
+            "invalid@[999.465.265.1]",
+            "test@.com",
+            "aaaaaaaaaaaaaaalongemailthatcausesregexDoSvulnerability@test.c"
+        ]
+    )
     fun `should throw IllegalArgumentException for invalid email`(invalidEmail: String) {
         val exception = assertThrows(IllegalArgumentException::class.java) {
             Email.of(invalidEmail)
