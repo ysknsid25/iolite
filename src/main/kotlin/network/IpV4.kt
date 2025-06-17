1c01027541d662f512d013d02063f2fe9857a2f0
ysknsid25 <kengo071225@gmail.com>
2025-06-17 14:47:37 +0900

    #21,57 - IPv4, IPv6

--- src/main/kotlin/network/IpV4.kt
+++ src/main/kotlin/network/IpV4.kt
@@ -9,12 +9,16 @@ value class IpV4(private val value: String) : ValueObject<String> {
     override fun parse(): String {
         val normalized = value.trim()
         require(
-            Regex(
-                "^(?:(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])\\.){3}(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])\$"
-            ).matches(normalized)
+            ipv4Regex.matches(normalized)
         ) {
             "Invalid IPV4 Address: $value"
         }
         return normalized
+    }
+
+    companion object {
+        private val ipv4Regex = Regex(
+            "^(?:(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])\\.){3}(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])\$"
+        )
     }
 }
